9661c527b8456ccfcbcf3923c935416665a5cf87
mikhail_shell <theoutstandingme@gmail.com>
2025-11-11 23:13:16 +0700

    nearly solved the task

--- main.cpp
+++ main.cpp
@@ -31,7 +31,7 @@ static map<string, QType> qtypes = {
 
 #pragma pack(push, 1)
 struct QuerySection {
-    char qName[255];
+    string qName;
     u_short qType, qClass;
 };
 #pragma pack(pop)
@@ -63,20 +63,19 @@ struct DnsRequest {
     QuerySection query;
     DnsRequest(u_short id, OpCode opcode, string host, QType qType) {
         header = {htons(id), htons((short)(0 << 15 | opcode << 14 | 1 << 8)), htons(1)};
-        query = {"", htons(qType), htons(1)}; // TODO htons
         string encodedHost = encodeName(host);
-        memcpy(&query.qName, encodedHost.c_str(), min((int)encodedHost.length(), 255));
+        query = {encodedHost, htons(qType), htons(1)};
     }
 };
 #pragma pack(pop)
 
 #pragma pack(push, 1)
 struct AnswerSection {
-    char name[255];
+    string name;
     u_short type, klass;
     u_int ttl;
     u_short rdLength;
-    char rData[65535];
+    string rData;
 };
 #pragma pack(pop)
 
@@ -88,10 +87,61 @@ struct DnsResponse {
 };
 #pragma pack(pop)
 
-string getPrettyPayload(char* payload) {
-    string payloadString = string(payload);
-    int terminatorPos = payloadString.find("\0");
-    return payloadString.substr(0, terminatorPos + 1);
+string getPrettyPayload(const string payload) {
+    string payloadString;
+    for (char ch : payload) {
+        payloadString += ch;
+        if (ch == '\0') break;
+    }
+    return payloadString;
+}
+
+string parseName(const char* buffer, u_int& offs) {
+    string name;
+    u_int offset = offs, originalOffset = offset;
+    bool jumped = false; // флаг, что был jump через pointer
+    const u_int MAX_JUMPS = 10; // защита от бесконечного цикла
+    u_int jumps = 0;
+
+    while (true) {
+        if (jumps++ > MAX_JUMPS) break; // защита
+        u_char len = buffer[offset];
+
+        // конец имени
+        if (len == 0) {
+            if (!jumped) offset++; // сдвигаем только если не было pointer
+            break;
+        }
+
+        // pointer compression: первые два бита = 11
+        if ((len & 0xC0) == 0xC0) {
+            u_char next = buffer[offset + 1];
+            u_int pointer = ((len & 0x3F) << 8) | next;
+            if (!jumped) offset += 2; // сдвигаем исходный offset один раз
+            offset = pointer; // меняем offset на позицию pointer
+            jumped = true;
+            continue;
+        }
+
+        // обычная метка
+        offset++;
+        if (!name.empty()) name += '.';
+        name.append(buffer + offset, len);
+        offset += len;
+    }
+
+    // если был jump, вернуть исходный offset + 2 (pointer)
+    if (jumped) {
+        return {name, originalOffset + 2};
+    }
+
+    return name;
+}
+
+string parsePayload(const char* responseBuffer, u_int offset, u_int length) {
+    string payload;
+    memcpy(payload.data(), responseBuffer + offset, length);
+    return payload;
 }
 
 void printUsage() {
@@ -139,36 +189,45 @@ int main (int argc, char** argv) {
 
     DnsRequest request = DnsRequest((u_short)1, (OpCode)0, host, qType);
     char requestBuffer[sizeof(DnsRequest)];
-    string prettyPayload = getPrettyPayload(request.query.qName);
-    u_int offset = 0, bytesToSend = sizeof(DnsRequest) - 255 + prettyPayload.size();
+    string payload = request.query.qName;
+    u_int offset = 0;
     memcpy(requestBuffer, &request.header, sizeof(DnsHeader));
     offset += sizeof(request.header);
-    memcpy(requestBuffer + offset, prettyPayload.c_str(), prettyPayload.size());
-    offset += prettyPayload.size();
+    memcpy(requestBuffer + offset, payload.c_str(), payload.size());
+    offset += payload.size();
     memcpy(requestBuffer + offset, &request.query.qType, sizeof(u_short));
     offset += sizeof(u_short);
     memcpy(requestBuffer + offset, &request.query.qClass, sizeof(u_short));
-    sendto(client, requestBuffer, bytesToSend, 0, (sockaddr*)&serverAddress, serverAddressSize);
+    offset += sizeof(u_short);
+    sendto(client, requestBuffer, offset, 0, (sockaddr*)&serverAddress, serverAddressSize);
 
     DnsResponse response;
     char responseBuffer[sizeof(DnsResponse)];
     int bytesReceived = recvfrom(client, responseBuffer, sizeof(DnsResponse), 0, (sockaddr*)&serverAddress, &serverAddressSize);
     offset = 0;
-    memcpy(&response, responseBuffer, sizeof(DnsHeader));
+    memcpy(&response.header, responseBuffer, sizeof(DnsHeader));
     offset += sizeof(DnsHeader);
-    u_int qNameEnd; // TODO: Find '\0'
-    memcpy(&response.query.qName, responseBuffer + offset, qNameEnd + 1);
-    offset += qNameEnd + 1;
-    memcpy(&response.query + qNameEnd + 1, responseBuffer + offset, 2 * sizeof(u_short));
-    offset += 2 * sizeof(u_short);
-    u_int nameEnd; // TODO: find '\0'
-    memcpy(&response.answer, responseBuffer + offset, nameEnd + 1);
-    memcpy(&response.answer + nameEnd + 1, responseBuffer + offset, 10);
-    offset += 10;
-    u_int rDataEnd; // TODO: find '\0'
-    memcpy(&response.answer + 10, responseBuffer + offset, rDataEnd + 1);
+    response.query.qName = parseName(responseBuffer, offset);
+    offset += response.query.qName.size();
+    memcpy(&response.query.qType, responseBuffer + offset, sizeof(u_short));
+    offset += sizeof(u_short);
+    memcpy(&response.query.qClass, responseBuffer + offset, sizeof(u_short));
+    offset += sizeof(u_short);
+    response.answer.name = parseName(responseBuffer, offset);
+    offset += response.answer.name.size();
+    memcpy(&response.answer.type, responseBuffer + offset, sizeof(u_short));
+    offset += sizeof(u_short);
+    memcpy(&response.answer.klass, responseBuffer + offset, sizeof(u_short));
+    offset += sizeof(u_short);
+    memcpy(&response.answer.ttl, responseBuffer + offset, sizeof(u_int));
+    offset += sizeof(u_int);
+    memcpy(&response.answer.rdLength, responseBuffer + offset, sizeof(u_short));
+    offset += sizeof(u_short);
+    response.answer.rData = parsePayload(responseBuffer, offset, response.answer.rdLength);
     cout << response.answer.rData << endl;
-
+    for (u_int i = 0; i < 1024; i++) {
+        printf("%02x", responseBuffer[i]);
+    }
     close(client);
     return 0;
 }
